2054bdfe4fb6f96a2b03c871055d7deb2ec413da
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-11-27 09:27:17 +0100

    Added error handling for mongoDb

--- GeoLocation.DAL/Repositories/IpAddressDetailsRepository.cs
+++ GeoLocation.DAL/Repositories/IpAddressDetailsRepository.cs
@@ -1,10 +1,10 @@
 ﻿using GeoLocation.BL.RepositoryInterfaces;
+using GeoLocation.DAL.Properties;
 using Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GeoLocation.DAL.Repositories
@@ -18,29 +18,80 @@ namespace GeoLocation.DAL.Repositories
             _context = context;
         }
 
-        public async Task Add(IpAddressDetails ipAddressDetail) => await _context.GeoLocations.InsertOneAsync(ipAddressDetail);
+        public async Task Add(IpAddressDetails ipAddressDetail)
+        {
+            try
+            {
+                await _context.GeoLocations.InsertOneAsync(ipAddressDetail);
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
+        }
 
-        public async Task<IEnumerable<IpAddressDetails>> GetAll() => await _context.GeoLocations.AsQueryable().ToListAsync();
+        public async Task<IEnumerable<IpAddressDetails>> GetAll()
+        {
+            try
+            {
+                return await _context.GeoLocations.AsQueryable().ToListAsync();
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
+        }
 
         public Task<IpAddressDetails> GetById(long id)
         {
-            FilterDefinition<IpAddressDetails> filter = Builders<IpAddressDetails>.Filter.Eq(ip => ip.Id, id);
-            return _context.GeoLocations.Find(filter).FirstOrDefaultAsync();
+            try
+            {
+                FilterDefinition<IpAddressDetails> filter = Builders<IpAddressDetails>.Filter.Eq(ip => ip.Id, id);
+                return _context.GeoLocations.Find(filter).FirstOrDefaultAsync();
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
         }
 
         public async Task<bool> Update(IpAddressDetails ipAddressDetail)
         {
-            var updateResult = await _context.GeoLocations.ReplaceOneAsync(ip => ip.Id == ipAddressDetail.Id, ipAddressDetail);
+            try
+            {
+                var updateResult = await _context.GeoLocations.ReplaceOneAsync(ip => ip.Id == ipAddressDetail.Id, ipAddressDetail);
 
-            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+                return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
         }
 
         public async Task<bool> Delete(long id)
         {
-            var deleteResult = await _context.GeoLocations.DeleteOneAsync(Builders<IpAddressDetails>.Filter.Eq(ip => ip.Id, id));
-            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+            try
+            {
+                var deleteResult = await _context.GeoLocations.DeleteOneAsync(Builders<IpAddressDetails>.Filter.Eq(ip => ip.Id, id));
+                return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
         }
 
-        public async Task<long> GetNextAvailableId() => await _context.GeoLocations.CountDocumentsAsync(new BsonDocument()) + 1;
+        public async Task<long> GetNextAvailableId()
+        {
+            try
+            {
+                return await _context.GeoLocations.CountDocumentsAsync(new BsonDocument()) + 1;
+            }
+            catch (Exception ex) when (ex is MongoException || ex is TimeoutException)
+            {
+                throw new ApplicationException(Resources.DatabaseUnavailableException, ex);
+            }
+        }
     }
 }
